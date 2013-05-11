6ecceba79402eb4feaecb9db2c5af93a163a13f4
Joe Bloggs <vapniks@yahoo.com>
2013-05-12 00:18:10 +0100

    Move bitcoin address to Commentary section

--- look-dired.el
+++ look-dired.el
@@ -39,14 +39,14 @@
 
 ;;; Commentary: 
 ;;
+;; Bitcoin donations gratefully accepted: 1ArFina3Mi8UDghjarGqATeBgXRDWrsmzo
+;;
 ;; This library provides extra commands for [[http://www.emacswiki.org/emacs/LookMode][look-mode]] (see below).
 ;; In addition if you mark some files in a dired buffer and then run look-at-file (or press M-l), 
 ;; all of the marked files will be visited in the *look* buffer.
 ;;;;
 
 ;;; Commands:
-;;
-;; Bitcoin donations gratefully accepted: 1ArFina3Mi8UDghjarGqATeBgXRDWrsmzo
 ;;
 ;; Below are complete command list:
 ;;
